4be0beb40950af61cb2de8b8fa2c7cbe4c0747d9
aljb <aljb@itu.dk>
2021-08-17 22:06:21 +0200

    Add basic chart

---
+++ src/chart.js
@@ -0,0 +1,57 @@
+const num_of_values = 50;
+
+function shuffleBars() {
+    const chart = document.getElementById("chart");
+    chart.innerHTML = "";
+
+    const height = chart.clientHeight / num_of_values;
+    const width = chart.clientWidth / num_of_values;
+    const arr = createArr(num_of_values);
+    
+    for (let i = 0; i < num_of_values; i++) {
+        const bar = createBar((num_of_values - arr[i]) * height, width);
+        chart.appendChild(bar);
+    }
+}
+
+function createBar(height, width) {
+    const bar = document.createElement("div");
+    bar.className = "bar";
+    
+    bar.style.height = `${height}px`;
+    bar.style.width = `${width}px`;
+
+    return bar;
+}
+
+function createArr(length) {
+    const arr = [];
+
+    for (let i = 0; i < length; i++) {
+        arr.push(i);
+    }
+
+    shuffle(arr);
+
+    return arr;
+}
+
+// https://stackoverflow.com/a/2450976/15207402
+function shuffle(arr) {
+    let currentIndex = arr.length;
+    let randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+    
+        // And swap it with the current element.
+        const temp = arr[currentIndex];
+        arr[currentIndex] = arr[randomIndex];
+        arr[randomIndex] = temp;
+    }
+  
+    return arr;
+}
