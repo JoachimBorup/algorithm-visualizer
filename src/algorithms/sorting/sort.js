78d79e9d65a6b2a449ebb55666ae4fa43314b2c9
aljb <aljb@itu.dk>
2021-08-20 21:34:09 +0200

    Remove unused arguments

--- src/algorithms/sorting/sort.js
+++ src/algorithms/sorting/sort.js
@@ -1,15 +1,15 @@
 class Sort {
     async less(i, j) {
-        markBars(i, j, 0.75);
+        markBars(i, j);
         await sleep(sleep_time);
-        unmarkBars(i, j, 1);
-        
+        unmarkBars(i, j);
+
         return getHeight(i) < getHeight(j);
     }
 
     async exch(i, j) {
         await sleep(sleep_time);
-        
+
         const temp = getHeight(i);
         setHeight(i, getHeight(j));
         setHeight(j, temp);
